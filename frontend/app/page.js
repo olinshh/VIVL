4fbcf189b517a0cbbc8bab9e477b4e08c5f97b96
men3laus <vs3006@nyu.edu>
2026-02-07 15:12:17 +0400

    created front-end folder, updated .gitignore

---
+++ frontend/app/page.js
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <div style={{ textAlign: 'center' }}>
+        <h1>Hello World!</h1>
+        <p>Welcome to VIVL Frontend</p>
+      </div>
+    </main>
+  )
+}
